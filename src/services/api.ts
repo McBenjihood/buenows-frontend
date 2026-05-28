f410936ab922ad4b36e5a60ea36e1c7f0ded739c
Benjamin Gerresheim <benjaminmikagerresheim@gmail.com>
2026-05-28 20:43:56 +0200

    - set correct api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig } from 'axios'
 import i18n from '@/i18n/index.ts'
 
-const API_BASE_URL = 'http://localhost:8080'
+const API_BASE_URL = 'https://api.bueno-ws.ch'
 const TOKEN_KEY = 'access_token'
 
 export function getLocalAccessToken(): string | null {
